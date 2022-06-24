1899d6cfbe7803034be9dc923914e08ea7135776
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-06-24 20:43:30 +0530

    sanity

---
+++ sanity-blog/schemas/sectionTwoPost.js
@@ -0,0 +1,83 @@
+export default {
+    name: 'postTwo',
+    title: 'Post Two',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        options: {
+          source: 'title',
+          maxLength: 96,
+        },
+      },
+      {
+        name: 'author',
+        title: 'Author',
+        type: 'reference',
+        to: {type: 'author'},
+      },
+      {
+        name: 'mainImage',
+        title: 'Main image',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: "description",
+        title: "Description",
+        type: "text"
+      },
+      {
+        name: 'categories',
+        title: 'Categories',
+        type: 'array',
+        of: [{type: 'reference', to: {type: 'category'}}],
+      },
+      {
+        name: 'publishedAt',
+        title: 'Published at',
+        type: 'datetime',
+      },
+      {
+        name: "tags",
+        type : "array",
+        of: [
+            {
+                type: "string",
+            },
+        ],
+        options: {
+            layout: "tags"
+        },
+      },
+      {
+        name: 'body',
+        title: 'Body',
+        type: 'blockContent',
+      },
+    ],
+  
+    preview: {
+      select: {
+        title: 'title',
+        author: 'author.name',
+        media: 'mainImage',
+      },
+      prepare(selection) {
+        const {author} = selection
+        return Object.assign({}, selection, {
+          subtitle: author && `by ${author}`,
+        })
+      },
+    },
+  }
+  
